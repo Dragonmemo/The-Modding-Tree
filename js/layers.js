049a7e5322e84057fb88ef2d7d7d679b714c388e
Dragonmemo <67173828+Dragonmemo@users.noreply.github.com>
2021-11-09 10:23:59 +0100

    Changing first layer

--- js/layers.js
+++ js/layers.js
@@ -1,20 +1,21 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("T255", {
+    name: "Tier 0", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "255", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    color: "#FFFFFF",
+    requires: new Decimal(1), // Can be a function that takes requirement increases into account
+    resource: "scraps", // Name of prestige currency
+    baseResource: "power", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 0, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
+        mult = Decimal.log10(mult)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -22,7 +23,7 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "s", description: "S: Dismantle the tree and get your scraps", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })

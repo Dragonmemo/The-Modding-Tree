d3ae332b1ffb577831e3ae5d5a63fff739b97941
Dragonmemo <67173828+Dragonmemo@users.noreply.github.com>
2021-11-11 18:52:38 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -54,3 +54,35 @@ addLayer("T255", {
     ],
     layerShown(){return true}
 })
+
+addLayer("Scraps", {
+    name: "Scraps", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#FFFFFF",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "scraps", // Name of prestige currency
+    baseResource: "power", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "custom",
+	getResetGain(this.layer){
+		return Decimal.log10(player.points)
+	},
+    //exponent: 1, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "s", description: "S: Dismantle the tree and get your scraps", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+})
